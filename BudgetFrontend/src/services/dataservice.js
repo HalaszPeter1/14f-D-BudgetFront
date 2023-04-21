810e7cbb6822c78bc99818c45659fa2d686f60b8
HalaszPeter1 <halasz.peter1@students.jedlik.eu>
2023-04-21 22:17:45 +0200

    fixed login

--- BudgetFrontend/src/services/dataservice.js
+++ BudgetFrontend/src/services/dataservice.js
@@ -4,7 +4,8 @@ const instance = Axios.create({
     baseURL: 'http://localhost:3000/api',
     headers:{
         'Content-Type' : 'application/json'
-    }
+    },
+    withCredentials:true
 });
 
 export default instance;
